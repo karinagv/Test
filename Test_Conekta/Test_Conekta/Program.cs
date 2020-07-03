213a713b1510ba635849091d1ad548e870f9c9ee
karinagv <67763014+karinagv@users.noreply.github.com>
2020-07-03 01:13:30 -0500

    Desarrollo de los métodos de la clase Matriz.

--- Test_Conekta/Test_Conekta/Program.cs
+++ Test_Conekta/Test_Conekta/Program.cs
@@ -2,14 +2,145 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Test_Conekta
 {
     class Program
     {
+        public static bool bFin = false;
+        public static bool bImageCreate = false;
+        public static bool bError = false;
+        public static cMatriz matriz ;
+
         static void Main(string[] args)
         {
+            Regex regex = new Regex(@"^\d+$");
+            Console.WriteLine("Bienvenido");
+            while (!bFin)
+            {
+                Console.WriteLine("Escribe el comando que deseas ejecutar");
+                string res = Console.ReadLine();
+                if (res == "X")
+                {
+                    bFin = true;
+                }
+                string[] command = res.Split(' ');
+                switch (command[0])
+                {
+                    case "I":
+                        bImageCreate = true;
+                        if (command.Length == 3)
+                        {
+                            if (regex.IsMatch(command[1]))
+                            {
+                                if (regex.IsMatch(command[2]))
+                                {
+                                    matriz = new cMatriz(Convert.ToInt32(command[1]), Convert.ToInt32(command[2]));
+                                }
+                                else
+                                {
+                                    bError = true;
+                                }
+                            }
+                            else
+                            {
+                                bError = true;
+                            }
+                           
+                            if (!bError)
+                            {
+
+                                bImageCreate = true;
+                                matriz.CrearMatriz();
+                                matriz.Imprimir();
+                            }
+                        }
+                        else
+                        {
+                            bError = true;
+                        }
+                        break;
+                    case "C":
+                        if (command.Length > 1 || !bImageCreate)
+                        {
+                            bError = true;
+                        }
+                        else
+                        {
+                            matriz.LimpiarMatriz();
+                            matriz.Imprimir();
+                        }
+                        break;
+                    case "L":
+                        if (command.Length == 4 && bImageCreate && regex.IsMatch(command[1]) && regex.IsMatch(command[2]))
+                        {
+                            matriz.funcionL(Convert.ToInt32(command[2]) - 1, Convert.ToInt32(command[1]) - 1, command[3]);
+                            matriz.Imprimir();
+                        }
+                        else
+                        {
+                            bError = true;
+                        }
+                        break;
+                    case "F":
+                        if (command.Length == 4 && bImageCreate && regex.IsMatch(command[1]) && regex.IsMatch(command[2]))
+                        {
+                            matriz.funcionF(Convert.ToInt32(command[2]) - 1, Convert.ToInt32(command[1]) - 1, command[3]);
+                            matriz.Imprimir();
+                        }
+                        else
+                        {
+                            bError = true;
+                        }
+                        break;
+                    case "H":
+                        if (command.Length == 5 && bImageCreate && regex.IsMatch(command[1]) && regex.IsMatch(command[3]) && regex.IsMatch(command[2]))
+                        {
+                            matriz.funcionH(Convert.ToInt32(command[2]) - 1, Convert.ToInt32(command[3]), Convert.ToInt32(command[1]), command[4]);
+                            matriz.Imprimir();
+                        }
+                        else
+                        {
+                            bError = true;
+                        }
+
+                        break;
+                    case "V":
+                        if (command.Length == 5 && bImageCreate && regex.IsMatch(command[1]) && regex.IsMatch(command[3]) && regex.IsMatch(command[2]))
+                        {
+                            matriz.funcionV(Convert.ToInt32(command[2]) - 1, Convert.ToInt32(command[3]), Convert.ToInt32(command[1]) - 1, command[4]);
+                            matriz.Imprimir();
+                        }
+                        else
+                        {
+                            bError = true;
+                        }
+
+                        break;
+                    case "S":
+                        if (command.Length == 1)
+                        {
+                            matriz.Imprimir();
+                        }
+                        else
+                            bError = true;
+                        break;
+                    default:
+                        bError = true;
+                        break;
+                }
+                if (bError)
+                {
+                    Console.WriteLine("Error. Comando No reconocido.");
+                    bError = false;
+                }
+                else
+                {
+                    Console.WriteLine("Comando ejecutado.");
+                }
+            }
         }
     }
 }
--- Test_Conekta/Test_Conekta/cMatriz.cs
+++ Test_Conekta/Test_Conekta/cMatriz.cs
@@ -16,48 +16,96 @@ namespace Test_Conekta
         }
 
         private int m;
-        public int _m
-        {
-            get { return _m; }
-            set { _m = value; }
-        }
+        //public int _m
+        //{
+        //    get { return _m; }
+        //    set { _m = value; }
+        //}
 
         private int n;
-        public int _n
+        //public int _n
+        //{
+        //    get { return _n; }
+        //    set { _n = value; }
+        //}
+
+
+
+        public cMatriz(int e, int e1)
         {
-            get { return _n; }
-            set { _n = value; }
+            m = e;
+            n = e1;
         }
 
-        private bool bFin;
-        public bool _bFin
+        public void CrearMatriz()
         {
-            get { return _bFin; }
-            set { _bFin = value; }
+            matriz = new cPixel[(int)n,(int) m];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[j, i] = new cPixel(j, i, "O");
+                }
+            }
         }
 
-
-        private bool bImageCreate;
-        public bool _bImageCreate
+        public void LimpiarMatriz()
         {
-            get { return _bImageCreate; }
-            set { _bImageCreate = value; }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matriz[j, i] = new cPixel(j, i, "O");
+                }
+            }
         }
 
-        private bool bError;
-        public bool _bError
+        public void funcionH(int c1, int c2, int c3, string color)
         {
-            get { return _bError; }
-            set { _bError = value; }
+            for (int j = c1; j < c2; j++)
+            {
+                matriz[j, c3 - 1].color = color;
+            }
         }
 
-        public cMatriz()
+        public void funcionV(int c1, int c2, int c3, string color)
         {
-            _m = 0;
-            _n = 0;
-            bFin = false;
-            bImageCreate = false;
-            bError = false;
+            for (int j = c1; j < c2; j++)
+            {
+                matriz[c3, j].color = color;
+            }
+        }
+
+        public void funcionF(int k1 , int j1, string color)
+        {
+            string colorBase = matriz[k1, j1].color;
+            matriz[k1,j1].color = color;
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matriz[j, i].color == colorBase)
+                        matriz[j, i] = new cPixel(j, i, color);
+                }
+            }
+        }
+
+        public void funcionL(int k, int j, string color)
+        {
+            matriz[k,j].color = color;
+        }
+
+        public void Imprimir()
+        {
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(
+                    matriz[j, i].color);
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
