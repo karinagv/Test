055c4f9853307365850fb8c443ef8505111a8448
karinagv <67763014+karinagv@users.noreply.github.com>
2020-07-03 00:21:58 -0500

    Clases principales del Test

---
+++ Test_Conekta/Test_Conekta/cMatriz.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Conekta
+{
+    class cMatriz
+    {
+        private cPixel[,] matriz;
+        public cPixel[,] _matriz
+        {
+            get { return _matriz; }
+            set { _matriz = value; }
+        }
+
+        private int m;
+        public int _m
+        {
+            get { return _m; }
+            set { _m = value; }
+        }
+
+        private int n;
+        public int _n
+        {
+            get { return _n; }
+            set { _n = value; }
+        }
+
+        private bool bFin;
+        public bool _bFin
+        {
+            get { return _bFin; }
+            set { _bFin = value; }
+        }
+
+
+        private bool bImageCreate;
+        public bool _bImageCreate
+        {
+            get { return _bImageCreate; }
+            set { _bImageCreate = value; }
+        }
+
+        private bool bError;
+        public bool _bError
+        {
+            get { return _bError; }
+            set { _bError = value; }
+        }
+
+        public cMatriz()
+        {
+            _m = 0;
+            _n = 0;
+            bFin = false;
+            bImageCreate = false;
+            bError = false;
+        }
+    }
+}
---
+++ Test_Conekta/Test_Conekta/cPixel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Conekta
+{
+    class cPixel
+    {
+        public cPixel(int i, int j, string col)
+        {
+            _M = i;
+            _N = j;
+            _color = col;
+        }
+        private int _M;
+        public int M
+        {
+            get { return _M; }
+            set { _M = value; }
+        }
+
+        private int _N;
+        public int N
+        {
+            get { return _N; }
+            set { _N = value; }
+        }
+
+        private string _color;
+        public string color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+
+    }
+}
